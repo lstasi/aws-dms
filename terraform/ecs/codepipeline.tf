38485a3e6b0ecc2b0e202d2a01fd33b1c94ec2e3
Leandro Stasi <leandro.stasi@gmail.com>
2021-04-28 12:31:20 +0200

    Move CodePipeline

--- terraform/ecs/codepipeline.tf
+++ terraform/ecs/codepipeline.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
 data "aws_caller_identity" "current" {}
 
 resource "aws_ecr_repository" "dms" {
