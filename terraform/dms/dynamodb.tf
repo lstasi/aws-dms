9a3ae2417e23e44614385b6b0557f2416bf1d6dd
Leandro Stasi <leandro.stasi@gmail.com>
2021-04-30 09:30:34 +0200

    Update Table Definition

--- terraform/dms/dynamodb.tf
+++ terraform/dms/dynamodb.tf
@@ -1,32 +1,19 @@
 resource "aws_dynamodb_table" "movies" {
-  name      = "Movies"
-  hash_key  = "Year"
-  range_key = "Title"
+  name         = "Movies"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "year"
+  range_key    = "title"
 
   attribute {
-    name = "Title"
-    type = "N"
+    name = "title"
+    type = "S"
   }
   attribute {
-    name = "Year"
+    name = "year"
     type = "N"
   }
 
   tags = {
     project = "dms"
   }
-}
-resource "aws_dynamodb_table_item" "movie-item" {
-  table_name = aws_dynamodb_table.movies.name
-  hash_key   = aws_dynamodb_table.movies.hash_key
-
-  item = <<ITEM
-{
-'year': {"N": "1999"},
-'title': {"S": "Matrix"},
-'info': {
-    'plot': {"S": "The Matrix"},
-    'rating': {"N": "5"}
-}
-ITEM
 }
